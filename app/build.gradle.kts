967ca88b579b576720dd2fe9bc04cfa282a686e4
mdabdullah98 <abdullah.md@seu.edu.bd>
2026-07-06 10:54:40 +0600

    fix: use Gradle 9.3.0 for Java 25 support with AGP 9.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesS
 
 plugins {
   alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
+  // alias(libs.plugins.kotlin.android)  // built-in with AGP 9.x
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
